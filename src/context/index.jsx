de3d323baa78ae4dba6b4a97401f2139b5dbcf71
aayubxon701 <aayubxon701@gmail.com>
2023-03-23 22:35:16 +0900

    root

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,13 +1,9 @@
-import { createContext } from "react";
 import PropertiProvider from "./properties";
 
-const Root = createContext();
 
 const RootContext = ({ children }) => {
   return (
-    <Root.Provider>
       <PropertiProvider>{children}</PropertiProvider>
-    </Root.Provider>
   );
 };
 
