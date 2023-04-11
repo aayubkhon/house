abfdbf6e1c8ce55e2e73b3ab05893a37a578aba5
aayubxon701 <aayubxon701@gmail.com>
2023-04-11 16:41:30 +0900

    navbar is ready

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {  Outlet, useNavigate } from "react-router-dom";
+import { Container, Logo, Section, Wrapper,Link } from "./style";
+import logoImg from "../../assets/logo/logo.svg";
+import { navbar } from "../../utils/nav";
+export const Navbar = () => {
+
+  const navigate = useNavigate()
+  return (
+    <Container>
+      <Outlet />
+      <Wrapper>
+        <Section onClick={()=>navigate('/home')} logo>
+          <Logo src={logoImg} alt="" />
+          <h2>Houzing</h2>
+        </Section>
+        <Section>
+          {navbar.map(({title, path},index) => {
+            return (
+              <Link className={({isActive})=> isActive && 'active'} key={index} to={path}>
+                {title}
+              </Link>
+            );
+          })}
+        </Section>
+        <Section>
+          <button>sign in</button>
+        </Section>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Navbar/style.js
@@ -0,0 +1,41 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+display:flex;
+justify-content:center;
+flex-direction:column;
+
+`
+
+const Wrapper = styled.div`
+display:flex;
+align-items:center;
+max-width:144opx;
+justify-content:space-between;
+height:64px;
+background:var(--colorPrimary);
+color:#ffff;
+padding:var(--padding);
+font-size:16px;
+`
+const Section = styled.div`
+display:flex;
+align-items:center;
+cursor:${({logo})=> logo && 'pointer'};
+.active{
+color: #5be952;
+}
+`
+
+const Logo = styled.img`
+margin:12px;
+width:30px;
+`
+
+const Link = styled(NavLink)`
+text-decoration:none;
+color:#ffff;
+padding:0 32px;
+`
+export{Container,Wrapper,Section,Logo,Link}
