7f9dd66fe8b282ae54809313cd643675f1e172a6
aayubxon701 <aayubxon701@gmail.com>
2023-04-11 16:41:30 +0900

    navbar is ready

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import { navbar } from "../utils/nav";
 
 export const Root = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<Navbar/>}>
         {
           navbar.map(({path,element,id})=>{
             return <Route key={id} path={path} element={element}/>
           })
         }
+        </Route>
         <Route path="*" element={<h1>404 NOT FOUND</h1>} />
         <Route path="/" element={<Navigate to={"/home"} />} />
       </Routes>
