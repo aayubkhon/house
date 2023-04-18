ffe686f5f14ab943baaf6b922540c389ba8eff14
aayubxon701 <aayubxon701@gmail.com>
2023-04-18 23:41:45 +0900

    house

--- src/components/HouseCard/style.js
+++ src/components/HouseCard/style.js
@@ -1,6 +1,10 @@
 import styled, { css } from "styled-components";
-
+import {ReactComponent as bed} from '../../assets/logo/bed.svg'
+import {ReactComponent as bath} from '../../assets/logo/bathh.svg'
+import {ReactComponent as car} from '../../assets/logo/car.svg'
+import {ReactComponent as ruler} from '../../assets/logo/ruler.svg'
 const common = css``;
+
 const Container = styled.div`
   width: 380px;
   height: 430px;
@@ -11,8 +15,34 @@ const Container = styled.div`
     drop-shadow(0px 8px 15px rgba(0, 0, 0, 0.06));
 `;
 
-const Img =styled.img`
-width:100%;
-max-height:220px;
-`
-export { Container,Img };
+const Img = styled.img`
+  width: 100%;
+  max-height: 220px;
+`;
+
+const Content = styled.div`
+  padding: 0 20px;
+  padding-top:24px;
+`;
+
+const Datails = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+Datails.Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  justify-content:center;
+  margin-top:20px;
+`;
+
+const Icons = styled.div`
+`;
+
+Icons.Bed = styled(bed)``;
+Icons.Bath = styled(bath)``;
+Icons.Car = styled(car)``;
+Icons.Ruler = styled(ruler)``;
+export { Container, Img, Content, Datails, Icons };
